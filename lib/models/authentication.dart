9150beee8b7d6b229566d35246dc3e37d64c4636
yazan sukkar <yazan.sukar@gmail.com>
2021-03-09 09:33:47 +0200

    first commit

---
+++ lib/models/authentication.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'http_exception.dart';
+import 'dart:convert';
+class Authentication with ChangeNotifier
+{
+  Future<void>  signUp(String email,String password)async
+  {
+    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDVeOsOmWJ4RdyFFpoYYfIvjXT5xbdXLXs';
+    try{
+      final response =  await http.post(url,body:json.encode(
+        {
+          'email':email,
+          'password': password,
+          'returnSecureToken':true,
+        }
+    ));
+    final responseData = json.decode(response.body);
+    if(responseData['error']!= null)
+    {
+      throw HttpException(responseData['error']['message']);
+    }
+   // print(responseData);
+
+    }catch(error)
+    {
+      throw error;
+    }
+   
+  }
+
+
+  Future<void>  logIn(String email,String password)async
+  {
+    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDVeOsOmWJ4RdyFFpoYYfIvjXT5xbdXLXs';
+    try{
+      final response =  await http.post(url,body:json.encode(
+        {
+          'email':email,
+          'password': password,
+          'returnSecureToken':true,
+        }
+    ));
+    final responseData = json.decode(response.body);
+    //print(responseData);
+    if(responseData['error']!= null)
+    {
+      throw HttpException(responseData['error']['message']);
+    }
+ 
+
+    }catch (error)
+    {
+      throw error;
+    }
+     }
+}
